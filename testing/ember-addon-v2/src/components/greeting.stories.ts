d100926fe400ea253a6991d97b35e29fdd1fbcd6
gossi <gossi@users.noreply.github.com>
2022-12-20 18:38:56 +0100

    Turn testing/ more into a real-life simulation

--- testing/ember-addon-v2/src/components/greeting.stories.ts
+++ testing/ember-addon-v2/src/components/greeting.stories.ts
@@ -7,7 +7,7 @@ export default {
 
 export const Default = (args: { hello: string; to: string }) => ({
   template: hbs`
-    <Greeting @hello={{this.hello}} @to={{this.to}}/>
+    <Greeting @hello={{this.hello}} @to={{this.to}} />
   `,
   context: args
 });
