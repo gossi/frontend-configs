7ace37d102ee20f7e5e8b66c6a80638f8fb20c9b
Thomas Gossmann <gossi@users.noreply.github.com>
2022-12-20 18:43:44 +0100

    Add `ember-template-lint-plugin-prettier` + config (#66)

--- packages/template-lint/index.js
+++ packages/template-lint/index.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  plugins: ['ember-template-lint-plugin-prettier'],
+  extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
+  overrides: [
+    {
+      files: ['tests/**/*-test.{js,ts}', '**/*.stories.{js,ts}', '**/stories.{js,ts}'],
+      rules: {
+        prettier: 'off'
+      }
+    }
+  ]
 };
