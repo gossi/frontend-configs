994b62ea2e43e57427d7274f6eb5d9ca75350485
gossi <gossi@users.noreply.github.com>
2023-07-26 20:03:27 +0200

    first look at modified configs

--- packages/eslint/configs/parsers/typescript.js
+++ packages/eslint/configs/parsers/typescript.js
@@ -1,7 +1,7 @@
 const typescriptParserOptions = {
   ecmaVersion: 'latest',
-  project: true
-  // extraFileExtensions: ['.gts']
+  project: true,
+  extraFileExtensions: ['.gts']
 };
 
 module.exports = { typescriptParserOptions };
