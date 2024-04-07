e528b0627ccaf5dd8cf2bab7306c0fa50a0c9450
gossi <gossi@users.noreply.github.com>
2024-04-07 16:45:16 +0200

    Naive and primitive types for `@storybook/ember` to model your stories

--- testing/ember-addon-v2-gts-stories/types/index.d.ts
+++ testing/ember-addon-v2-gts-stories/types/index.d.ts
@@ -1,2 +1,8 @@
 import 'ember-source/types';
 import 'ember-source/types/preview';
+
+import type AddonRegistry from '../src/template-registry';
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry extends AddonRegistry {}
+}
