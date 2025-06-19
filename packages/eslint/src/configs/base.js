a3e4e99a2a943a667d9d7e9c008eeeba49e844d5
gossi <gossi@users.noreply.github.com>
2025-06-19 10:37:10 +0200

    Switch to `lilconfig`

--- packages/eslint/src/configs/base.js
+++ packages/eslint/src/configs/base.js
@@ -7,37 +7,39 @@ import noConsole from './rules/no-console.js';
 import paddingLineBetweenStatements from './rules/padding-line-between-statements.js';
 import { resolvePrettier } from './rules/prettier.js';
 
-export default [
-  /**
-   * Ignores must be in their own object
-   * https://eslint.org/docs/latest/use/configure/ignore
-   */
-  {
-    name: 'gossi/base/ignores',
-    ignores: ['dist/', 'tmp/', 'declarations/', 'coverage/', '!**/.*']
-  },
-  /**
-   * https://eslint.org/docs/latest/use/configure/configuration-files#configuring-linter-options
-   */
-  {
-    name: 'gossi/base/linter-options',
-    linterOptions: {
-      reportUnusedDisableDirectives: 'error'
-    }
-  },
-  /**
-   * Configs
-   */
-  ...resolvePrettier(),
-  js.configs.recommended,
-  noConsole,
-  paddingLineBetweenStatements,
-  importSorting,
-  base,
-  ...imports
-  // {
-  //   // name: 'gossi/base/imports',
-  //   files: ['**/*.js', '**/*.mjs', '**/*.cjs', '**/*.ts'],
-  //   extends: []
-  // }
-];
+export function config(root) {
+  return [
+    /**
+     * Ignores must be in their own object
+     * https://eslint.org/docs/latest/use/configure/ignore
+     */
+    {
+      name: 'gossi/base/ignores',
+      ignores: ['dist/', 'tmp/', 'declarations/', 'coverage/', '!**/.*']
+    },
+    /**
+     * https://eslint.org/docs/latest/use/configure/configuration-files#configuring-linter-options
+     */
+    {
+      name: 'gossi/base/linter-options',
+      linterOptions: {
+        reportUnusedDisableDirectives: 'error'
+      }
+    },
+    /**
+     * Configs
+     */
+    ...resolvePrettier(root),
+    js.configs.recommended,
+    noConsole,
+    paddingLineBetweenStatements,
+    importSorting,
+    base,
+    ...imports
+    // {
+    //   // name: 'gossi/base/imports',
+    //   files: ['**/*.js', '**/*.mjs', '**/*.cjs', '**/*.ts'],
+    //   extends: []
+    // }
+  ];
+}
