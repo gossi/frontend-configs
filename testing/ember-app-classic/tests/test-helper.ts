38142a236acf5ba7c6fb3bcbf67e94a5bd7e8d5a
gossi <gossi@users.noreply.github.com>
2025-06-19 17:25:39 +0200

    Update to `ember-qunit@9`

--- testing/ember-app-classic/tests/test-helper.ts
+++ testing/ember-app-classic/tests/test-helper.ts
@@ -2,6 +2,7 @@ import { setApplication } from '@ember/test-helpers';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
 
 import Application from 'ember-app-classic/app';
 import config from 'ember-app-classic/config/environment';
@@ -11,4 +12,5 @@ setApplication(Application.create(config.APP));
 // eslint-disable-next-line import-x/namespace
 setup(QUnit.assert);
 
+loadTests();
 start();
