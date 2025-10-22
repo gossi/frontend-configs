c3eb1ad4d2c8e8f82cb221cbea078f45e61b98e4
gossi <gossi@users.noreply.github.com>
2025-10-22 15:47:02 +0200

    Update Template Lint Rules

--- packages/template-lint/src/index.js
+++ packages/template-lint/src/index.js
@@ -3,6 +3,16 @@ import prettierPlugin from 'ember-template-lint-plugin-prettier';
 export default {
   plugins: [prettierPlugin],
   extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
+  rules: {
+    'no-negated-condition': false,
+    'no-passed-in-event-handlers': {
+      ignore: {
+        Form: ['submit'],
+        Pagination: ['change']
+      }
+    },
+    'no-forbidden-elements': ['meta', 'html', 'script']
+  },
   overrides: [
     {
       files: ['tests/**/*-test.{js,ts}', '**/*.stories.{js,ts}', '**/stories.{js,ts}'],
